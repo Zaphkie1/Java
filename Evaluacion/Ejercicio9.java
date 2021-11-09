94521cc17392761b26aa37d5a48921f322bed174
Zaphkie1 <89409775+Zaphkie1@users.noreply.github.com>
2021-11-08 21:05:11 -0600

    eliminacion

--- Evaluacion/Ejercicio9.java
+++
@@ -1,8 +0,0 @@
-package Evaluacion;
-
-public class Ejercicio9 {
-    public static void main(String[] args) {
-        //Declaracion de variables
-        System.out.println("hola mundo");
-    }
-}
