855784176e8f4d68670fbc8a6dde96af5076fd8a
Zaphkie1 <89409775+Zaphkie1@users.noreply.github.com>
2021-11-10 12:52:06 -0600

    Nuevo y Cambios

--- repeticion/ParImparDoWhile.java
+++ repeticion/ParImparDoWhile.java
@@ -15,32 +15,15 @@ public class ParImparDoWhile {
         int numero,i,sPares=0,sImpares=0;
         int npar=0,total,nimpar=0;
         Scanner in = new Scanner(System.in);
-        //solicitar los datos de entrada
-        System.out.println("\tSumatoria de Numeros Pares e Nmpares\n");
-        System.out.println("--------------------------------------\n");
-        System.out.print("Ingresa total de numeros que deseas capturar: ");
-        total=in.nextInt();
-        i=1;//inicializacion
-        while(i <= total)//condicion
+        System.out.println("\tNumero Par-Impar");
+        System.out.println("---------------------------");
+        System.out.println("Ingresa un numero positivo o negativo: ");
+        numero = in.nextInt();
+        do {
+            
+        } while (numero > 0);
         {
-            System.out.print("Ingresa valor: "+i+" ==> ");
-            numero=in.nextInt();
-            if (numero %2 == 0)
-            {
-                sPares+=numero; //sumatoria de los pares
-                npar++; //cuenta los numeros pares
-            }
-            else
-            {
-                sImpares+=numero;
-                nimpar++;
-            }
-            i++;
-        }// fin del while
-        System.out.println("------------------------------------------\n");
-        System.out.println("La sumatoria de "+npar+" numeros"
-        +" pares es "+sPares);
-        System.out.println("La sumatoria de "+nimpar+" numeros"
-        +" pares es "+sImpares);
-    }
-}
+            System.out.println("Ingresa otro numero: ");
+        }
+    }//Fin del metodo main
+}//Fin de la clase
