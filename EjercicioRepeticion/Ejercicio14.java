470a9917ba8398bb6f3b34e99accbf9368c83824
Zaphkie1 <89409775+Zaphkie1@users.noreply.github.com>
2021-11-20 15:13:48 -0600

    Ejercicios Repeticion

---
+++ EjercicioRepeticion/Ejercicio14.java
@@ -0,0 +1,5 @@
+package EjercicioRepeticion;
+
+public class Ejercicio14 {
+    
+}
--- repeticion/MultiplosFor.java
+++ repeticion/MultiplosFor.java
@@ -9,22 +9,5 @@ o multiplo de 5, mostrar los 5 numeros
 public class MultiplosFor {
     public static void main(String[] args) {
         // Declaracion de variables
-        Scanner in = new Scanner(System.in);
-        int n = 0;
-        String datos = "";
-        for (int x = 1; x <= 5; x++) {
-            System.out.print("Introduce un numero: ");
-            n = in.nextInt();
-            if (n % 5 == 0) {
-                System.out.println("El numero " + n + " es multilo de 5");
-            } if (n % 3 == 0) {
-                System.out.println("El numero " + n + " es multiplo de 3");
-            }
-            datos += n + "\t";
         }
-        System.out.println("\tMultilos de 3 y 5");
-        System.out.println("------------------------------------");
-        System.out.print("Nnmeros ingresados \n");
-        System.out.print(datos);
-       }
     }
