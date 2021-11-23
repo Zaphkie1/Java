cbd8624391141ad38950559fbe4df048bc54ec79
Zaphkie1 <89409775+Zaphkie1@users.noreply.github.com>
2021-11-22 21:49:23 -0600

    EJ 21 - 23 completados

--- EjercicioRepeticion/Ejercicio21.java
+++ EjercicioRepeticion/Ejercicio21.java
@@ -1,4 +1,7 @@
 package EjercicioRepeticion;
+
+import java.util.Scanner;
+
 /*
 Ej 21: Realiza un programa que vaya pidiendo números hasta que se introduzca un número
 negativo y nos diga cuantos números se han introducido, la media de los impares y el mayor
@@ -7,6 +10,36 @@ datos pero no se incluye en el cómputo.
 */
 public class Ejercicio21 {
     public static void main(String[] args) {
-        
-    }
-}
+        //Declaracion de variables
+        int numeroIntroducido;
+        int numeroDeElementos = 0;
+        int sumaImpares = 0;
+        int numeroDeElementosImpares = 0;
+        int maximoPar = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("-------------------------------------");
+        System.out.println("Media de impares y mayor de pares");
+        System.out.println("-------------------------------------");
+        System.out.println("Instrucciones: Introduce numeros enteros, Para terminar ingresa un numero negativo ");
+        System.out.println("-------------------------------------");
+        do {
+            System.out.println("Introduce un numero: ");
+            numeroIntroducido = in.nextInt();
+
+            if (numeroIntroducido >= 0) {
+                numeroDeElementos++;
+                if ((numeroIntroducido % 2) == 1) { // número impar
+                    sumaImpares += numeroIntroducido;
+                    numeroDeElementosImpares++;
+                } else {
+                    if (numeroIntroducido > maximoPar)
+                        maximoPar = numeroIntroducido;
+                }
+            }
+        } while (numeroIntroducido >= 0);
+
+        System.out.println("Ha introducido " + numeroDeElementos + " números");
+        System.out.println("La media de los impares es " + sumaImpares / numeroDeElementosImpares);
+        System.out.println("El máximo de los pares es " + maximoPar);
+    }// Fin del metodo main ()
+}// Fin de la clase
--- EjercicioRepeticion/Ejercicio22.java
+++ EjercicioRepeticion/Ejercicio22.java
@@ -4,6 +4,23 @@ Ej 22: Muestra por pantalla todos los números primos entre 2 y 100, ambos inclu
 */
 public class Ejercicio22 {
     public static void main(String[] args) {
-        
+        System.out.println("-------------------------------------");
+        System.out.println("Numeros primos estre 2 y 100");
+        System.out.println("-------------------------------------");
+    boolean esPrimo = true;
+    int n,i;
+    for (n = 2; n <= 100; n++) {
+      // comprueba si n es primo
+      esPrimo = true;
+      for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+          esPrimo = false;
+        }
+      }
+      if (esPrimo) {
+        System.out.print(n + " ");
+      }
+    }
+    System.out.println();
     }
 }
