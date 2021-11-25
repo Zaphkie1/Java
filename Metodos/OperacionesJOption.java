37c833263aeb336d2287fbaa1dc5e6946e767297
Zaphkie1 <89409775+Zaphkie1@users.noreply.github.com>
2021-11-25 12:52:47 -0600

    Nuevo

---
+++ Metodos/OperacionesJOption.java
@@ -0,0 +1,9 @@
+package Metodos;
+/*
+Ej:6
+*/
+public class OperacionesJOption {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Metodos/PromedioEstudianteJOption.java
@@ -0,0 +1,9 @@
+package Metodos;
+/*
+Ej7:
+*/
+public class PromedioEstudianteJOption {
+    public static void main(String[] args) {
+        
+    }
+}
