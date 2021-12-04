f79f6cbbdf22ebd59e645a39d4d3187f8e0620dc
Zaphkie1 <89409775+Zaphkie1@users.noreply.github.com>
2021-12-03 20:23:12 -0600

    Completdo

--- GuiaMetodosyArreglos/Ejercicio1.java
+++ GuiaMetodosyArreglos/Ejercicio1.java
@@ -1,10 +1,23 @@
 package GuiaMetodosyArreglos;
+
 /*
 Ej1:Escribe un programa que muestre la tirada de tres dados. Se debe mostrar también
 la suma total (los puntos que suman entre los tres dados).
 */
 public class Ejercicio1 {
     public static void main(String[] args) {
-        
-    }
+        // Declaracion de variables
+        int dado1,dado2,dado3,resultado;
+        System.out.println("-------------------------------------");
+        System.out.println("\t\tDado");
+        System.out.println("-------------------------------------");
+        dado1 =((int)(Math.random()*6)+1);
+        dado2 =((int)(Math.random()*6)+1);
+        dado3 =((int)(Math.random()*6)+1);
+        resultado=dado1+dado2+dado3;
+        System.out.println("Dado 1 = "+dado1);
+        System.out.println("Dado 2 = "+dado2);
+        System.out.println("Dado 3 = "+dado3);
+        System.out.println("Suma de los dados = "+resultado);
+    }// Fin del whitch
 }
--- GuiaMetodosyArreglos/Ejercicio2.java
+++ GuiaMetodosyArreglos/Ejercicio2.java
@@ -5,6 +5,11 @@ separados por espacios.
 */
 public class Ejercicio2 {
     public static void main(String[] args) {
-        
+        System.out.println("-------------------------------------");
+        System.out.println("Numeros aleatorios entre 1 y 100");
+        System.out.println("-------------------------------------");
+        for (int i = 1; i <= 100000; i++) {
+            System.out.print((int) (Math.random() * 100 + 1) + " \t");
+        }
     }
 }
